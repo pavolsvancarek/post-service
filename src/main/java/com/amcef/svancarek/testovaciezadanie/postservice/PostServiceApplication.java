e5b0fb89d222ab43fe8b9ce72c7d1fa1efb4fe71
Pavol <palinosvancarek@gmail.com>
2024-05-21 14:13:08 +0200

    Initial commit

---
+++ src/main/java/com/amcef/svancarek/testovaciezadanie/postservice/PostServiceApplication.java
@@ -0,0 +1,13 @@
+package com.amcef.svancarek.testovaciezadanie.postservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amcef/svancarek/testovaciezadanie/postservice/PostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amcef.svancarek.testovaciezadanie.postservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
